b736265802e1a09d05c9845e317a95c25685c554
GarryLed <s00256356@atu.ie>
2024-04-13 09:47:43 +0100

    Working on Q4

---
+++ Q4/BankAccount.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Write a class to represent objects of type BankAccount.  It should have the following private attributes :
+Account Number (integer).  This should be unique for all objects of type BankAccount.
+Customer Name (string)
+Account Type (string ) (allowed types : Current, Deposit, Savings or invalid). 
+BIC (bank identification code) (string)
+Balance (decimal)
+Account PIN  (string- 4 digits)
+
+Create properties (getter and setter) for these as follows:
+Balance – getter only
+Account type- use the setter to validate that it is an allowed type or throw a format exception. 
+Name – getter, private setter
+BIC- getter, private setter
+PIN – private setter, private getter
+
+
+Add the following functionality to the class:
+Lodge money
+Withdraw money
+Check Balance
+Change PIN
+Modify 2,3,and 4 to include use of PIN
+ */
+
+namespace Q4
+{
+    internal class BankAccount
+    {
+        // attributes 
+        private string _name;
+        private string _accountType; // only allows 3 types of bank account; Current, Deposit, Savings (all else is invalid) 
+        private string _bic; // how do I create a BIC?
+        private decimal _balance;
+        private string _accountPin; // 4 digit 
+
+        // static variable available to the entire class 
+        private static int _accountNumberSeed = 1234567890; // number will be incremented for each new object 
+        public static string bic = "BOFIIE2D"; // set the BIC code 
+       
+
+        // properties 
+        // Account number property 
+        public int AccountNumber
+        {
+            get
+            {
+                return _accountNumberSeed;
+            }
+        }
+
+        // Balance property 
+
+        public decimal Balance { get { return _balance; } } // get only  
+
+        // Account type property 
+        public string AccountType
+        {
+            get { return _accountType; }
+            set 
+            {
+                if (value == "Current" || value == "Deposit" || value == "Savings")
+                {
+                    _accountType = value; // how to I use this to create a valid account type? 
+                }
+                else
+                {
+                    // prompt user to enter a valid option 
+                }
+            }
+        }
+        // name property 
+        public string Name 
+        { 
+            get 
+            { return _name; 
+            }
+            set // how do I make thi setter private? 
+            {
+                _name = value;
+            }
+        }
+        // BIC property 
+        public string Bic
+        {
+            get
+            {
+                return _bic;
+            }
+            set // how do i make this private? 
+            {
+                _bic = value; 
+            }
+        }
+
+        // PIN property 
+        private string AccountPin
+        {
+            get
+            {
+                return _accountPin;
+            }
+            set
+            {
+                _accountPin = value;
+            }
+        }
+
+        // parameterised constructor 
+        public BankAccount(string name, string pin )
+        {
+            _balance = 0; // initial balance is set to 0 
+            Name = name;
+            AccountPin = pin;
+            Bic = bic; // set bic to public static variable 
+            _accountNumberSeed++; // increment this number by one for each new objcet 
+        }
+
+        // instance methods 
+        // Lodge money method 
+        public decimal LodgeMoney(decimal amount)
+        {
+            // write validation logic here for 0 and negative numbers 
+            _balance += amount;
+            return amount;
+        }
+        // withdraw money method 
+        public decimal WithdrawMoney(decimal amount) // method needs input validaation 
+        {
+            if (amount < _balance)
+            {
+               _balance -= amount;
+            }
+            return amount;
+        }
+        // chagne pin method 
+        public string ChangePin()
+        {
+
+        }
+
+
+        // method to print attributes of an object 
+        public override string ToString()
+        {
+            return "Balance: " + Balance; // prints the balance 
+        }
+    }
+}
---
+++ Q4/Program.cs
@@ -0,0 +1,67 @@
+﻿/*
+* Name: Garry Ledwith
+* Date: 13/04/24
+* Description:
+* Worksheet 8, Q4
+* 
+Create a new project for this question.
+Write a class to represent objects of type BankAccount.  It should have the following private attributes :
+Account Number (integer).  This should be unique for all objects of type BankAccount.
+Customer Name (string)
+Account Type (string ) (allowed types : Current, Deposit, Savings or invalid). 
+BIC (bank identification code) (string)
+Balance (decimal)
+Account PIN  (string- 4 digits)
+Create properties (getter and setter) for these as follows:
+Balance – getter only
+Account type- use the setter to validate that it is an allowed type or throw a format exception. 
+Name – getter, private setter
+BIC- getter, private setter
+PIN – private setter, private getter
+Add the following functionality to the class:
+Lodge money
+Withdraw money
+Check Balance
+Change PIN
+Modify 2,3,and 4 to include use of PIN
+
+Write a Main() method in the program.cs class to use the BankAccount class Here you will
+Create a BankAccount object. Instantiate the BankAccount objects by obtaining the necessary attributes from a user through keyboard input. 
+
+Write a menu that allows the user to 
+1. Lodge
+2. Withdraw
+3. Check Balance
+4. Change Pin
+5. Exit
+
+*/
+namespace Q4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // create a new instantce of the class 
+            BankAccount account = new BankAccount("Garry", "1234");
+            Console.WriteLine(account.Name);
+            Console.WriteLine(account.AccountNumber);
+            Console.WriteLine(account.ToString()); // initial balance is 0 
+            Console.WriteLine(account.Bic);
+
+            Console.WriteLine(account.LodgeMoney(5)); // make a lodgement (amount is 5)
+                                                      // 
+            Console.WriteLine(account.ToString()); // balance is now 5
+
+            Console.WriteLine(account.WithdrawMoney(3)); // withdraw money (amount is  3) 
+
+            Console.WriteLine(account.ToString()); // balance is now 2 
+
+            // create a new instance of the class 
+            BankAccount account2 = new BankAccount("Beata", "1234");
+            Console.WriteLine(account2.Name);
+            Console.WriteLine(account2.AccountNumber);
+            Console.WriteLine(account2.Balance);
+        }
+    }
+}
